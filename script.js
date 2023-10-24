4a402461d13991f20ce956423a750a53a4f4d314
pratamaa112 <hanung112@gmail.com>
2023-10-24 22:57:41 +0700

    five

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const navigation = document.querySelector('.navbar');
 document.querySelector('#hamburger-menu').onclick = () => {
     navigation.classList.toggle('active')
 };
+
+//klik diluar side bar hambburger-menu hilang
+const hambuger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click',function(e){
+    if(!hambuger.contains(e.target) && !navigation.contains(e.target)){
+        navigation.classList.remove('active')
+    }
+})
