d3847e3bbabcf3826d2db020f1ba31dcb621db4b
pratamaa112 <hanung112@gmail.com>
2023-10-27 20:20:08 +0700

    2nd

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 const navigation = document.querySelector('.navbar');
 
 // ketika hambubrger-menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-    navigation.classList.toggle('active')
-};
+ document.querySelector('#hamburger-menu').onclick = () => {
+     navigation.classList.toggle('active')
+ };
 
 //klik diluar side bar hambburger-menu hilang
 const hambuger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click',function(e){
+    if(!hambuger.contains(e.target) && !navigation.contains(e.target)){
+        navigation.classList.remove('active')
+    }
+})
+
+//ketika klik navbar list - nav hilang
+document.querySelector('#navbars').onclick = () => {
+    navigation.classList.toggle('active')
+};
+
+const navbar = document.querySelector('#navbars');
 
 document.addEventListener('click',function(e){
     if(!hambuger.contains(e.target) && !navigation.contains(e.target)){
